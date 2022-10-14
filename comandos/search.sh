ecf7934db90e9e6550a0a1241a9cb05faf57a703
BrayanTCc83 <btellez948@gmail.com>
2022-10-13 23:17:20 -0500

    [Fix][Brayan] Corrigiendo funcionamiento de search, agregando recursividad.

--- comandos/search.sh
+++ comandos/search.sh
@@ -1,28 +1,44 @@
 #!/bin/bash
 tmpFile="file.txt"
+fResult="result.txt"
 path="$1"
-maxRec=10
+fname=$2
+flag=$3
 
 function split(){
     echo $9
 }
 
-directory=$1
-fname=$2
-ls -l $directory > $tmpFile
-files=()
-directories=()
-maxRec=$(($maxRec-1))
-while read line; do
-    file=$(echo $line | grep '^-')
-    if [ ! -z "$file" ] 
-    then
-        name=$(split $line)
-        result=$(echo $name | grep $fname)
-        if [ ! -z "$result" ] 
+function searchFile {
+    directory=$1
+    ls -l $directory > $tmpFile
+    files=()
+    directories=()
+
+    while read line; do
+        file=$(echo $line | grep '^-')
+        if [ ! -z "$file" ] 
         then
-            echo "$path/$result"
+            name=$(split $line)
+            result=$(echo $name | grep $fname)
+            if [ ! -z "$result" ] 
+            then
+                echo "$directory $result" >> $fResult
+            fi
+        elif [[ ! -z $(echo $line | grep '^d') ]] && [ "$flag" == "-r" ]
+        then
+            directories+=("$directory/$(echo $line | cut -c46-)")
         fi
-    fi
-done < $tmpFile
+    done < $tmpFile
+
+    for dir in ${directories[@]}
+    do
+        searchFile $dir $fname $flag
+    done
+}
+
+searchFile $path $fname $flag
+cat $fResult
+
 rm $tmpFile
+rm $fResult
