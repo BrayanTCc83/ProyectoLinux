2943818066d11ad17c72d9d7d246dc52abc078a3
BrayanTCc83 <btellez948@gmail.com>
2022-10-13 22:49:56 -0500

    [Feature][Brayan] Agregue la terminal dentro del sistema con los comandos posibles.

--- help.sh
+++ help.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-commands=("help" "search" "os" "datetime" "credits" "game" "mp3")
+commands=("help" "search" "infosys" "datetime" "credits" "game" "mp3")
 commandInfo=(
     "---- HELP ----\n> Este comando te brinda informacion sobre los comandos dentro del programa.\n\thelp [command]\nINFO:\n\tcommand: Nombre del comando a usar.\n\t\thelp [-flag]\nFLAGS:\n\t-l: Lista los comandos existentes.\n\t-a: Muestra los comandos con sus definiciones.\n"
     "---- SEARCH ----\n> Permite buscar un archivo en un directorio especifico.\n\tsearch [path] [fileName] [-flag]\nINFO:\n\tpath: Ruta especificada.\n\tfileName: Nombre del archivo, nombre incompleto o extension del archivo a buscar.\nFLAGS:\n\t-r: Busca de forma recursiva dentro de las carpetas del directorio indicado.\n"
-    "---- OS ----\n> Muestra la informacion basica del sistema operativo, Memoria RAM, version del sistema y arquitectura.\n\t os\n"
+    "---- INFOSYS ----\n> Muestra la informacion basica del sistema operativo, Memoria RAM, version del sistema y arquitectura.\n\t os\n"
     "---- DATETIME ----\n> Indica el uso horario, fecha y hora actual.\n\tdatetime\n\tdatetime [use]\nINFO:\n\tuse: Especifica el uso horario que se desee obtener entre UTF-12 hasta UTF+12, si no se indica, muestra la hora actual en tu uso horario.\n"
     "---- CREDITS ----\n> Muestra los nombres y creditos de los creadores. \n\tcredits\n\tcredits [developer] [-flag]\nINFO:\n\tdeveloper: Nombre del desarrollador\nFLAGS:\n\t-e: Indica de forma extendida el desarrollo que realizo el creador.\n\t-a: Muestra informacion sobre la experiencia del desarrollo.\n"
     "---- GAME ----\n> Abre un juego de gato (3 en ralla) dentro de la terminal.\n\tgame\n"
--- terminal.sh
+++ terminal.sh
@@ -1,2 +1,64 @@
 #!/bin/bash
 
+commands=("help" "search" "infosys" "datetime" "credits" "game" "mp3" "clear" "exit")
+
+function execute(){
+    params=()
+    for i in $@
+    do
+        if [[ " ${commands[*]} " =~ " $i " ]]
+        then 
+            command=$i
+        else
+            params+=($i)
+        fi
+    done
+    case $command in
+        ${commands[0]})
+            ./help.sh ${params[@]}
+        ;;
+        ${commands[1]})
+            ./comandos/search.sh ${params[@]}
+        ;;
+        ${commands[2]})
+            ./comandos/os.sh ${params[@]}
+        ;;
+        ${commands[3]})
+            ./comandos/fecha.sh ${params[@]}
+        ;;
+        ${commands[4]})
+            ./comandos/credits.sh ${params[@]}
+        ;;
+        ${commands[5]})
+            ./Juego/gato.sh ${params[@]}
+        ;;
+        ${commands[6]})
+            ./help.sh ${params[@]}
+        ;;
+        ${commands[7]})
+            clear
+        ;;
+        ${commands[8]})
+            exit
+        ;;
+    esac
+}
+
+clear
+while [ true ]
+do
+    terminalEntry="\e[0;1;36m[$(pwd)] > \e[0;0;37m"
+    echo -n -e "$terminalEntry"
+    read command
+
+    words=($command)
+    if [ ${#words[@]} != 0 ]
+    then 
+        if [[ " ${commands[*]} " =~ " ${words[0]} " ]]
+        then
+            execute "$(echo ${words[@]})"
+        else
+            echo "El comando que ingresaste no existe en la lista de comandos, para mas informacion escribe help -l."
+        fi
+    fi
+done
